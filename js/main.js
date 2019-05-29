5298cb7d1da9d23744b4f42c90dc81f8a9517355
Aliosha <aliosha.shalabay@gmai.com>
2019-05-29 12:28:46 +0300

    block sample and adaptive

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
-$("input[type='number']").inputSpinner()
+$("input[type='number']").inputSpinner();
+
+jQuery(document).ready(function () {
+    $(".slider").slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }],
+    });
+});
